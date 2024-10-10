2a090346bf5e3702f63ddfad7030656ed1a00694
Vimanshani <pabodhavimanshani@gmail.com>
2024-10-10 23:53:54 +0530

    ..

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -18,7 +18,11 @@ namespace ChessUI
     public partial class MainWindow : Window
     {
         private readonly Image[,] pieceImages = new Image[8, 8];
+        private readonly Rectangle[,] highlight = new Rectangle[8,8];  
+        private readonly Dictionary<Position, Move> MoveCache = new Dictionary<Position, Move>();
+
         private GameState gameState;
+        private Position selectedPos = null;
         public MainWindow()
         {
             InitializeComponent();
